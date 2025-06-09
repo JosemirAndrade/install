42851f3e966ec6d6321c40369d2a491d596efbb3
Josemir_Andrade <114767293+JosemirAndrade@users.noreply.github.com>
2025-06-09 16:37:04 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh  
+
+sudo apt install gnupg software-properties-common -y
+sudo mkdir -m755 -p /etc/apt/keyrings  # not needed since apt version 2.4.0 like Debian 12 and Ubuntu 22 or newer 
+sudo wget -O /etc/apt/keyrings/qgis-archive-keyring.gpg https://download.qgis.org/downloads/qgis-archive-keyring.gpg -y
+
+sudo apt update
+
+sudo apt install qgis qgis-plugin-grass -y
